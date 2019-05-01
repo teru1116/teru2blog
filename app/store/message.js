0fb12288624c9d64e5156485b220c50de49f14e8
Teruya Kusumoto <teruyakusumoto@gmail.com>
2019-05-01 23:16:37 +0700

    pass spec test: update message.status

--- app/store/message.js
+++ app/store/message.js
@@ -9,12 +9,22 @@ const getters = {
 const mutations = {
   addMessage (state, payload) {
     state.list.set(payload.id, payload.message)
+  },
+
+  sendMessage (state, payload) {
+    const message = state.list.get(payload.id)
+    message.status = 'sent'
+    state.list.set(payload.id, message)
   }
 }
 
 const actions = {
   addMessage ({ commit }, { id, message }) {
     commit('addMessage', { id, message })
+  },
+
+  sendMessage ({ commit }, { id, message}) {
+    commit('sendMessage', { id, message })
   }
 }
 
