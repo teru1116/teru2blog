ad8827d0d6b5b1a10ce076d6ca135dd2e9dccd99
Teruya Kusumoto <teruyakusumoto@gmail.com>
2019-05-05 12:29:22 +0700

    UI: admin layout

---
+++ app/layouts/admin.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <header>
+      <div class="inner">
+        <div class="header-left-wrapper">
+          <h1>管理画面</h1>
+        </div>
+        <div class="header-right-wrapper">
+          <nav>
+            <ul>
+              <li>
+                <nuxt-link to="articles">記事</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="articles">チャット</nuxt-link>
+              </li>
+              <li>
+                <button>ログアウト</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <main>
+      <div class="inner">
+        <nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+header {
+  .inner {
+    display: flex;
+    justify-content: space-between;
+    .header-left-wrapper {
+      h1 {
+        color: #333;
+      }
+    }
+    .header-right-wrapper {
+      nav {
+        ul {
+          display: flex;
+          align-items: center;
+          height: 48px;
+          li {
+            margin-left: 16px;
+            a, button {
+              color: #757575;
+              font-size: 14px;
+              font-weight: 400;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
