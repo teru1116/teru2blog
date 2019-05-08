574d6625ae5bcb508fac2ee26c9739351408c697
Teruya Kusumoto <teruyakusumoto@gmail.com>
2019-05-08 15:27:49 +0700

    add spec test: post and save article

--- test/spec/admin/article.spec.js
+++ test/spec/admin/article.spec.js
@@ -1,8 +1,9 @@
 import Vuex from 'vuex'
-import article from './../../../app/store/article'
+import article from './../../../app/store/admin/article'
 import { createLocalVue } from '@vue/test-utils'
 import cloneDeep from 'clone-deep'
 import firebase from './../../../app/plugins/firebase'
+import v4 from 'uuid/v4'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -14,14 +15,15 @@ describe('store/admin/article', () => {
   beforeAll(async () => {
     store = new Vuex.Store(cloneDeep(article))
     db = firebase.firestore()
+    const testArticleCount = 25
 
     // initialize DB
-    // テスト用の記事を予め25件登録しておく
+    // テスト用の記事を予め登録しておく
     const batch = db.batch()
-    for (let index = 0; index < 25; index++) {
+    for (let index = 0; index < testArticleCount; index++) {
       const article = {
-        title: `ARTICLE_${25 - index}`,
-        createdDate: new Date(new Date().getTime() - (25 - index) * 24 * 60 * 60 * 1000),
+        title: `ARTICLE_${testArticleCount - index}`,
+        createdDate: new Date(new Date().getTime() - (testArticleCount - index) * 24 * 60 * 60 * 1000),
         isTestData: true
       }
       const ref = db.collection('articles').doc()
@@ -41,9 +43,45 @@ describe('store/admin/article', () => {
   test('初回リクエストを行うと、記事が20件ストアに保持されていること', async () => {
     expect(store.getters['articles'].length).toBe(0)
 
-    await store.dispatch('fetchArticles')
+    await store.dispatch('fetchAllArticles')
 
     expect(store.getters['articles'].length).toBe(20)
+  })
+
+  test('記事を投稿すると、DBに登録されていること', async () => {
+    const beforeSnapshot = await db.collection('articles').where('isTestData', '==', true).get()
+    expect(beforeSnapshot.size).toBe(testArticleCount)
+
+    const articleId = v4()
+    store.dispatch('updateTitle', 'TITLE')
+    store.dispatch('updateContentHTML', '<p>CONTENT</p>')
+    store.dispatch('selectCategory', 'CATEGORY')
+    await store.dispatch('postArticle', articleId)
+
+    const doc = await db.collection('articles').doc(articleId).get()
+    const article = doc.data()
+    expect(article.isPublished).toBeTruthy()
+    expect(article.title).toBe('TITLE')
+    expect(article.contentHTML).toBe('<p>CONTENT</p>')
+    expect(article.categories).toContain('CATEGORY')
+  })
+
+  test('記事を下書き保存すると、DBに下書きで登録されていること', () => {
+    const snapshot = await db.collection('articles').where('isTestData', '==', true).get()
+    expect(snapshot.size).toBe(testArticleCount)
+
+    const articleId = v4()
+    store.dispatch('updateTitle', 'TITLE')
+    store.dispatch('updateContentHTML', '<p>CONTENT</p>')
+    store.dispatch('selectCategory', 'CATEGORY')
+    await store.dispatch('saveArticle', articleId)
+
+    const doc = await db.collection('articles').doc(articleId).get()
+    const article = doc.data()
+    expect(article.isPublished).toBeFalsy()
+    expect(article.title).toBe('TITLE')
+    expect(article.contentHTML).toBe('<p>CONTENT</p>')
+    expect(article.categories).toContain('CATEGORY')
   })
 
   afterAll(async () => {
