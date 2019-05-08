7037f3b749c5670c9877598313adf134d574c063
Teruya Kusumoto <teruyakusumoto@gmail.com>
2019-05-09 00:43:30 +0700

    UI: article list item

--- app/components/ArticleListItem.vue
+++ app/components/ArticleListItem.vue
@@ -3,17 +3,22 @@
     <a>
       <div class="article-list-item-left">
         <!-- アイキャッチ画像 -->
-        <div class="article-image-icatch" />
+        <div class="article-image-icatch" :style="{ backgroundImage: `url(${article.icatchImageDataURL})` }" />
       </div>
       <div class="article-list-item-right">
         <!-- 記事タイトル -->
         <h3>{{ article.title }}</h3>
-        <!-- 記事本文 -->
-        <p>{{ article.body || 'また、ドキュメント スナップショットをクエリカーソルの開始点または終了点としてカーソル句に渡すこともできます。ドキュメント スナップショットの値は、クエリカーソルの値として使用されます。' }}</p>
-        <!-- カテゴリー -->
-        <small>{{ categoriesString }}</small>
-        <!-- 投稿日 -->
-        <time>{{ dateString }}</time>
+        <p v-if="!$isMobile">{{ contentString }}</p>
+        <div class="metadata">
+          <!-- カテゴリー -->
+          <ul class="category-list">
+            <li v-for="(categoryName, index) in article.categories" :key="index">
+              {{ categoryName }}
+            </li>
+          </ul>
+          <!-- 投稿日 -->
+          <time>{{ dateString }}</time>
+        </div>
       </div>
     </a>
     <hr />
@@ -23,20 +28,12 @@
 <script>
 export default {
   props: {
-    article: {
-      type: Object,
-      default: {
-        icatchImageURL: '',
-        title: '記事タイトル',
-        body: '',
-        categories: {
-          '1qazxsw2': { name: '技術' }
-        },
-        createdDate: new Date()
-      }
-    }
+    article: Object
   },
   computed: {
+    contentString () {
+      return this.article.contentHTML.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').slice(0, 100)
+    },
     dateString () {
       const createdDate = this.article.createdDate.toDate()
       return `${createdDate.getFullYear()}.${createdDate.getMonth() + 1}.${createdDate.getDate()}`
@@ -61,16 +58,77 @@ li {
     display: flex;
     padding: 16px 0;
     .article-list-item-left {
-      width: 180px;
       margin-right: 16px;
+      @media screen and (min-width: 600px) {
+        width: 180px;
+      }
+      @media screen and (max-width: 599px) {
+        width: 30vw;
+      }  
       .article-image-icatch {
-        height: 135px;
-        background-color: #0070c9;
-        background: linear-gradient(#42a1ec, #0070c9);
+        @media screen and (min-width: 600px) {
+          height: 135px;
+        }
+        @media screen and (max-width: 599px) {
+          height: calc(30vw * 0.75);
+        }
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-color: whitesmoke;
       }
     }
     .article-list-item-right {
       flex: 1;
+      position: relative;
+      h3 {
+        font-size: 18px;
+        font-weight: bold;
+        line-height: 1.1;
+        margin-bottom: 16px;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 3;
+        overflow: hidden;
+      }
+      p {
+        color: #333;
+        font-size: 12px;
+        line-height: 1.76;
+      }
+      .metadata {
+        display: flex;
+        position: absolute;
+        bottom: 0;
+        ul.category-list {
+          display: flex;
+          @media screen and (min-width: 600px) {}
+          @media screen and (max-width: 599px) {}
+          li {
+            margin-right: 4px;
+            color: #0052A3;
+            font-size: 15px;
+            display: inline-flex;
+            &:after {
+              content: ",";
+              color: #000;
+            }
+            &:last-child {
+              &:after {
+                content: "";
+              }
+            }
+          }
+          &:after {
+            content: " | ";
+          }
+        }
+        time {
+          margin-left: 8px;
+          color: #555;
+          font-weight: bold;
+        }
+      }
     }
   }
   hr {
@@ -78,6 +136,7 @@ li {
     background: linear-gradient(#42a1ec, #0070c9);
     height: 1px;
     border: 0;
+    margin: 0;
   }
 }
 </style>
