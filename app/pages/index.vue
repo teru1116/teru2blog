d2022558a9cb966267e4062dc1c2d47ca906dffc
Teruya Kusumoto <teruyakusumoto@gmail.com>
2019-05-09 18:47:11 +0700

    add: /articles section.category

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    <!-- 最新記事 -->
-    <section id="article-list-recent">
-      <div class="inner">
-        <h2>
-          最新記事
-          <hr/>
-        </h2>
-        <ol>
-          <ArticleListItem
-            v-for="(article, index) in article.recentArticles"
-            :key="index"
-            :article="article"
-          />
-        </ol>
-      </div>
-    </section>
+    <div class="inner">
+      <!-- 最新記事 -->
+      <section id="article-list-recent">
+          <h2>
+            最新記事
+            <hr/>
+          </h2>
+          <ol>
+            <ArticleListItem
+              v-for="(article, index) in articles"
+              :key="index"
+              :article="article"
+            />
+          </ol>
+      </section>
+      <aside>
+        <!-- カテゴリー -->
+        <section id="category-list">
+          <h2>
+            カテゴリー
+            <hr/>
+          </h2>
+          <ul>
+            <li v-for="(categoryName, index) in categories" :key="index">
+              <nuxt-link :to="`/categories/${categoryName}`">{{ categoryName }}</nuxt-link>
+            </li>
+          </ul>
+        </section>
+      </aside>
+    </div>
     <!-- チャットでお問い合わせ -->
     <div class="chatwidget-parent">
       <ChatWidget/>
@@ -24,9 +38,9 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import ArticleListItem from './../components/ArticleListItem'
 import ChatWidget from './../components/ChatWidget'
+import { mapState } from 'vuex';
 
 export default {
   components: {
@@ -34,9 +48,10 @@ export default {
     ChatWidget
   },
   computed: {
-    ...mapState([
-      'article'
-    ]),
+    ...mapState({
+      articles: state => state.article.recentArticles,
+      categories: state => state.category.categories
+    })
   },
   async created () {
     // ブラウザ識別
@@ -45,12 +60,60 @@ export default {
 
     // 記事取得
     this.$store.dispatch('article/fetchRecentArticles')
+
+    // カテゴリー取得
+    this.$store.dispatch('category/fetchAllCategory')
   },
   layout: 'site'
 }
 </script>
 
 <style lang="scss" scoped>
+.inner {
+  @media screen and (min-width: 600px) {
+    display: flex;
+  }
+  @media screen and (max-width: 599px) {}
+  section#article-list-recent {
+    @media screen and (min-width: 600px) {
+      flex: 1;
+    }
+    @media screen and (max-width: 599px) {}
+  }
+  aside {
+    @media screen and (min-width: 600px) {
+      width: 200px;
+      margin-left: 40px;
+    }
+    @media screen and (max-width: 599px) {}
+    h2 {
+      font-size: 14px;
+      font-weight: bold;
+    }
+  }
+}
+section {
+  @media screen and (min-width: 600px) {
+    margin-bottom: 48px;
+  }
+  @media screen and (max-width: 599px) {
+    margin-bottom: 60px;
+  }
+}
+section#category-list {
+  ul {
+    margin: 16px 0 0;
+    li {
+      a {
+        color: #0052A3;
+        padding: 2px 0;
+        display: inline-block;
+        @media screen and (min-width: 600px) {}
+        @media screen and (max-width: 599px) {}
+      }
+    }
+  }
+}
 .chatwidget-parent {
   position: fixed;
   bottom: 0;
---
+++ app/store/category.js
@@ -0,0 +1,62 @@
+import firebase from './../plugins/firebase'
+
+const db = firebase.firestore()
+
+const defaultState = () => {
+  return {
+    categories: [],
+    articles: []
+  }
+}
+
+const state = defaultState()
+
+const getters = {
+  //
+}
+
+const mutations = {
+  setArticles (state, payload) {
+    state.articles = payload
+  },
+
+  setCategories (state, payload) {
+    state.categories = payload
+  },
+
+  clearState (state) {
+    state = Object.assign(state, defaultState())
+  }
+}
+
+const actions = {
+  async fetchAllCategory ({ commit }) {
+    let results = []
+    const snapshot = await db.collection('articleCategories').get()
+    snapshot.forEach(doc => {
+      results.push(doc.id)
+    })
+    commit('setCategories', results)
+  },
+
+  async fetchArticles ({ commit }, categoryName) {
+    console.log(categoryName)
+    let results = []
+    const snapshot = await db.collection('articles').where('categories', 'array-contains',categoryName).orderBy('createdDate', 'desc').limit(10).get()
+    snapshot.forEach(doc => {
+      results.push(Object.assign({ id: doc.id }, doc.data()))
+    })
+    commit('setArticles', results)
+  },
+
+  clearState ({ commit }) {
+    commit('clearState')
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
