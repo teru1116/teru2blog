9b49776bad504345cfadace41e623c9973211c62
Teruya Kusumoto <teruyakusumoto@gmail.com>
2019-05-09 23:19:49 +0700

    add: page /monthly/:month

---
+++ app/pages/monthly/_month.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="inner">
+    <section id="article-list-recent">
+      <h2>{{ `${year}年${month}月` }}<hr/></h2>
+      <ol>
+        <ArticleListItem
+          v-for="(article, index) in articles"
+          :key="index"
+          :article="article"
+        />
+      </ol>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import ArticleListItem from './../../components/ArticleListItem'
+
+export default {
+  components: {
+    ArticleListItem
+  },
+  computed: {
+    ...mapState({
+      articles: state => state.monthly.articles
+    }),
+    year () {
+      return Number(this.$route.params.month.substr(0, 4))
+    },
+    month () {
+      return Number(this.$route.params.month.substr(5, 2))
+    }
+  },
+  methods: {
+    fetchArticles () {
+      this.$store.dispatch('monthly/fetchArticles', { year: this.year, month: this.month })
+    }
+  },
+  created () {
+    this.fetchArticles()
+  },
+  layout: 'site'
+}
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
+
